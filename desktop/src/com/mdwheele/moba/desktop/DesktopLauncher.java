f34212ac00eca6f288926eabe80c843c6e8b520a
Dustin Wheeler <mdwheele@ncsu.edu>
2016-03-01 22:51:55 -0500

    Initial commit of LibGdx project

---
+++ desktop/src/com/mdwheele/moba/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mdwheele.moba.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mdwheele.moba.MobaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MobaGame(), config);
+	}
+}
